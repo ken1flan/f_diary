7e214528aa54d27c2972f0497376b74f18bf5963
ken1flan <ken1flan@gmail.com>
2021-12-05 16:50:58 +0900

    Update page when return
    編集画面から戻ったときに、
    画面を更新するようにします。
    
    Navigator.pushをawaitして、
    戻ってきたときにsetState()で更新します。

--- lib/widgets/my_home_page.dart
+++ lib/widgets/my_home_page.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:f_diary/widgets/article_page.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Center(
         child: Text(DateTime.now().toString()),
@@ -25,11 +30,12 @@ class MyHomePage extends StatelessWidget {
     );
   }
 
-  void _openArticle(BuildContext context) {
-    Navigator.push(
+  void _openArticle(BuildContext context) async {
+    await Navigator.push(
         context,
         MaterialPageRoute(
           builder: (context) => ArticlePage(DateTime.now()),
         ));
+    setState(() {});
   }
 }
