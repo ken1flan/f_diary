6ea8fba1be0bd46b69744027bfba4656d1042ebf
ken1flan <ken1flan@gmail.com>
2022-01-30 09:03:59 +0900

    Articleモデルの追加

---
+++ lib/models/article.g.dart
@@ -0,0 +1,681 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'article.dart';
+
+// **************************************************************************
+// IsarCollectionGenerator
+// **************************************************************************
+
+// ignore_for_file: duplicate_ignore, non_constant_identifier_names, invalid_use_of_protected_member
+
+extension GetArticleCollection on Isar {
+  IsarCollection<Article> get articles {
+    return getCollection('Article');
+  }
+}
+
+final ArticleSchema = CollectionSchema(
+  name: 'Article',
+  schema:
+      '{"name":"Article","properties":[{"name":"body","type":"String"},{"name":"createdAt","type":"Long"},{"name":"title","type":"String"},{"name":"updatedAt","type":"Long"}],"indexes":[],"links":[]}',
+  adapter: const _ArticleAdapter(),
+  idName: 'id',
+  propertyIds: {'body': 0, 'createdAt': 1, 'title': 2, 'updatedAt': 3},
+  indexIds: {},
+  indexTypes: {},
+  linkIds: {},
+  backlinkIds: {},
+  linkedCollections: [],
+  getId: (obj) => obj.id,
+  setId: (obj, id) => obj.id = id,
+  getLinks: (obj) => [],
+  version: 0,
+);
+
+class _ArticleAdapter extends IsarTypeAdapter<Article> {
+  const _ArticleAdapter();
+
+  @override
+  int serialize(IsarCollection<Article> collection, IsarRawObject rawObj,
+      Article object, List<int> offsets,
+      [int? existingBufferSize]) {
+    rawObj.id = object.id ?? Isar.autoIncrement;
+    var dynamicSize = 0;
+    final value0 = object.body;
+    final _body = BinaryWriter.utf8Encoder.convert(value0);
+    dynamicSize += _body.length;
+    final value1 = object.createdAt;
+    final _createdAt = value1;
+    final value2 = object.title;
+    final _title = BinaryWriter.utf8Encoder.convert(value2);
+    dynamicSize += _title.length;
+    final value3 = object.updatedAt;
+    final _updatedAt = value3;
+    final size = dynamicSize + 34;
+
+    late int bufferSize;
+    if (existingBufferSize != null) {
+      if (existingBufferSize < size) {
+        isarFree(rawObj.buffer);
+        rawObj.buffer = isarMalloc(size);
+        bufferSize = size;
+      } else {
+        bufferSize = existingBufferSize;
+      }
+    } else {
+      rawObj.buffer = isarMalloc(size);
+      bufferSize = size;
+    }
+    rawObj.buffer_length = size;
+    final buffer = bufAsBytes(rawObj.buffer, size);
+    final writer = BinaryWriter(buffer, 34);
+    writer.writeBytes(offsets[0], _body);
+    writer.writeDateTime(offsets[1], _createdAt);
+    writer.writeBytes(offsets[2], _title);
+    writer.writeDateTime(offsets[3], _updatedAt);
+    return bufferSize;
+  }
+
+  @override
+  Article deserialize(IsarCollection<Article> collection, int id,
+      BinaryReader reader, List<int> offsets) {
+    final object = Article();
+    object.body = reader.readString(offsets[0]);
+    object.createdAt = reader.readDateTime(offsets[1]);
+    object.id = id;
+    object.title = reader.readString(offsets[2]);
+    object.updatedAt = reader.readDateTime(offsets[3]);
+    return object;
+  }
+
+  @override
+  P deserializeProperty<P>(
+      int id, BinaryReader reader, int propertyIndex, int offset) {
+    switch (propertyIndex) {
+      case -1:
+        return id as P;
+      case 0:
+        return (reader.readString(offset)) as P;
+      case 1:
+        return (reader.readDateTime(offset)) as P;
+      case 2:
+        return (reader.readString(offset)) as P;
+      case 3:
+        return (reader.readDateTime(offset)) as P;
+      default:
+        throw 'Illegal propertyIndex';
+    }
+  }
+}
+
+extension ArticleQueryWhereSort on QueryBuilder<Article, Article, QWhere> {
+  QueryBuilder<Article, Article, QAfterWhere> anyId() {
+    return addWhereClause(const WhereClause(indexName: null));
+  }
+}
+
+extension ArticleQueryWhere on QueryBuilder<Article, Article, QWhereClause> {
+  QueryBuilder<Article, Article, QAfterWhereClause> idEqualTo(int? id) {
+    return addWhereClause(WhereClause(
+      indexName: null,
+      lower: [id],
+      includeLower: true,
+      upper: [id],
+      includeUpper: true,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterWhereClause> idNotEqualTo(int? id) {
+    if (whereSortInternal == Sort.asc) {
+      return addWhereClause(WhereClause(
+        indexName: null,
+        upper: [id],
+        includeUpper: false,
+      )).addWhereClause(WhereClause(
+        indexName: null,
+        lower: [id],
+        includeLower: false,
+      ));
+    } else {
+      return addWhereClause(WhereClause(
+        indexName: null,
+        lower: [id],
+        includeLower: false,
+      )).addWhereClause(WhereClause(
+        indexName: null,
+        upper: [id],
+        includeUpper: false,
+      ));
+    }
+  }
+
+  QueryBuilder<Article, Article, QAfterWhereClause> idGreaterThan(
+    int? id, {
+    bool include = false,
+  }) {
+    return addWhereClause(WhereClause(
+      indexName: null,
+      lower: [id],
+      includeLower: include,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterWhereClause> idLessThan(
+    int? id, {
+    bool include = false,
+  }) {
+    return addWhereClause(WhereClause(
+      indexName: null,
+      upper: [id],
+      includeUpper: include,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterWhereClause> idBetween(
+    int? lowerId,
+    int? upperId, {
+    bool includeLower = true,
+    bool includeUpper = true,
+  }) {
+    return addWhereClause(WhereClause(
+      indexName: null,
+      lower: [lowerId],
+      includeLower: includeLower,
+      upper: [upperId],
+      includeUpper: includeUpper,
+    ));
+  }
+}
+
+extension ArticleQueryFilter
+    on QueryBuilder<Article, Article, QFilterCondition> {
+  QueryBuilder<Article, Article, QAfterFilterCondition> bodyEqualTo(
+    String value, {
+    bool caseSensitive = true,
+  }) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.eq,
+      property: 'body',
+      value: value,
+      caseSensitive: caseSensitive,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> bodyGreaterThan(
+    String value, {
+    bool caseSensitive = true,
+    bool include = false,
+  }) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.gt,
+      include: include,
+      property: 'body',
+      value: value,
+      caseSensitive: caseSensitive,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> bodyLessThan(
+    String value, {
+    bool caseSensitive = true,
+    bool include = false,
+  }) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.lt,
+      include: include,
+      property: 'body',
+      value: value,
+      caseSensitive: caseSensitive,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> bodyBetween(
+    String lower,
+    String upper, {
+    bool caseSensitive = true,
+    bool includeLower = true,
+    bool includeUpper = true,
+  }) {
+    return addFilterCondition(FilterCondition.between(
+      property: 'body',
+      lower: lower,
+      includeLower: includeLower,
+      upper: upper,
+      includeUpper: includeUpper,
+      caseSensitive: caseSensitive,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> bodyStartsWith(
+    String value, {
+    bool caseSensitive = true,
+  }) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.startsWith,
+      property: 'body',
+      value: value,
+      caseSensitive: caseSensitive,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> bodyEndsWith(
+    String value, {
+    bool caseSensitive = true,
+  }) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.endsWith,
+      property: 'body',
+      value: value,
+      caseSensitive: caseSensitive,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> bodyContains(
+      String value,
+      {bool caseSensitive = true}) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.contains,
+      property: 'body',
+      value: value,
+      caseSensitive: caseSensitive,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> bodyMatches(
+      String pattern,
+      {bool caseSensitive = true}) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.matches,
+      property: 'body',
+      value: pattern,
+      caseSensitive: caseSensitive,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> createdAtEqualTo(
+      DateTime value) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.eq,
+      property: 'createdAt',
+      value: value,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> createdAtGreaterThan(
+    DateTime value, {
+    bool include = false,
+  }) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.gt,
+      include: include,
+      property: 'createdAt',
+      value: value,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> createdAtLessThan(
+    DateTime value, {
+    bool include = false,
+  }) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.lt,
+      include: include,
+      property: 'createdAt',
+      value: value,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> createdAtBetween(
+    DateTime lower,
+    DateTime upper, {
+    bool includeLower = true,
+    bool includeUpper = true,
+  }) {
+    return addFilterCondition(FilterCondition.between(
+      property: 'createdAt',
+      lower: lower,
+      includeLower: includeLower,
+      upper: upper,
+      includeUpper: includeUpper,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> idIsNull() {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.isNull,
+      property: 'id',
+      value: null,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> idEqualTo(int? value) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.eq,
+      property: 'id',
+      value: value,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> idGreaterThan(
+    int? value, {
+    bool include = false,
+  }) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.gt,
+      include: include,
+      property: 'id',
+      value: value,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> idLessThan(
+    int? value, {
+    bool include = false,
+  }) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.lt,
+      include: include,
+      property: 'id',
+      value: value,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> idBetween(
+    int? lower,
+    int? upper, {
+    bool includeLower = true,
+    bool includeUpper = true,
+  }) {
+    return addFilterCondition(FilterCondition.between(
+      property: 'id',
+      lower: lower,
+      includeLower: includeLower,
+      upper: upper,
+      includeUpper: includeUpper,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> titleEqualTo(
+    String value, {
+    bool caseSensitive = true,
+  }) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.eq,
+      property: 'title',
+      value: value,
+      caseSensitive: caseSensitive,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> titleGreaterThan(
+    String value, {
+    bool caseSensitive = true,
+    bool include = false,
+  }) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.gt,
+      include: include,
+      property: 'title',
+      value: value,
+      caseSensitive: caseSensitive,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> titleLessThan(
+    String value, {
+    bool caseSensitive = true,
+    bool include = false,
+  }) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.lt,
+      include: include,
+      property: 'title',
+      value: value,
+      caseSensitive: caseSensitive,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> titleBetween(
+    String lower,
+    String upper, {
+    bool caseSensitive = true,
+    bool includeLower = true,
+    bool includeUpper = true,
+  }) {
+    return addFilterCondition(FilterCondition.between(
+      property: 'title',
+      lower: lower,
+      includeLower: includeLower,
+      upper: upper,
+      includeUpper: includeUpper,
+      caseSensitive: caseSensitive,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> titleStartsWith(
+    String value, {
+    bool caseSensitive = true,
+  }) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.startsWith,
+      property: 'title',
+      value: value,
+      caseSensitive: caseSensitive,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> titleEndsWith(
+    String value, {
+    bool caseSensitive = true,
+  }) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.endsWith,
+      property: 'title',
+      value: value,
+      caseSensitive: caseSensitive,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> titleContains(
+      String value,
+      {bool caseSensitive = true}) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.contains,
+      property: 'title',
+      value: value,
+      caseSensitive: caseSensitive,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> titleMatches(
+      String pattern,
+      {bool caseSensitive = true}) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.matches,
+      property: 'title',
+      value: pattern,
+      caseSensitive: caseSensitive,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> updatedAtEqualTo(
+      DateTime value) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.eq,
+      property: 'updatedAt',
+      value: value,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> updatedAtGreaterThan(
+    DateTime value, {
+    bool include = false,
+  }) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.gt,
+      include: include,
+      property: 'updatedAt',
+      value: value,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> updatedAtLessThan(
+    DateTime value, {
+    bool include = false,
+  }) {
+    return addFilterCondition(FilterCondition(
+      type: ConditionType.lt,
+      include: include,
+      property: 'updatedAt',
+      value: value,
+    ));
+  }
+
+  QueryBuilder<Article, Article, QAfterFilterCondition> updatedAtBetween(
+    DateTime lower,
+    DateTime upper, {
+    bool includeLower = true,
+    bool includeUpper = true,
+  }) {
+    return addFilterCondition(FilterCondition.between(
+      property: 'updatedAt',
+      lower: lower,
+      includeLower: includeLower,
+      upper: upper,
+      includeUpper: includeUpper,
+    ));
+  }
+}
+
+extension ArticleQueryWhereSortBy on QueryBuilder<Article, Article, QSortBy> {
+  QueryBuilder<Article, Article, QAfterSortBy> sortByBody() {
+    return addSortByInternal('body', Sort.asc);
+  }
+
+  QueryBuilder<Article, Article, QAfterSortBy> sortByBodyDesc() {
+    return addSortByInternal('body', Sort.desc);
+  }
+
+  QueryBuilder<Article, Article, QAfterSortBy> sortByCreatedAt() {
+    return addSortByInternal('createdAt', Sort.asc);
+  }
+
+  QueryBuilder<Article, Article, QAfterSortBy> sortByCreatedAtDesc() {
+    return addSortByInternal('createdAt', Sort.desc);
+  }
+
+  QueryBuilder<Article, Article, QAfterSortBy> sortById() {
+    return addSortByInternal('id', Sort.asc);
+  }
+
+  QueryBuilder<Article, Article, QAfterSortBy> sortByIdDesc() {
+    return addSortByInternal('id', Sort.desc);
+  }
+
+  QueryBuilder<Article, Article, QAfterSortBy> sortByTitle() {
+    return addSortByInternal('title', Sort.asc);
+  }
+
+  QueryBuilder<Article, Article, QAfterSortBy> sortByTitleDesc() {
+    return addSortByInternal('title', Sort.desc);
+  }
+
+  QueryBuilder<Article, Article, QAfterSortBy> sortByUpdatedAt() {
+    return addSortByInternal('updatedAt', Sort.asc);
+  }
+
+  QueryBuilder<Article, Article, QAfterSortBy> sortByUpdatedAtDesc() {
+    return addSortByInternal('updatedAt', Sort.desc);
+  }
+}
+
+extension ArticleQueryWhereSortThenBy
+    on QueryBuilder<Article, Article, QSortThenBy> {
+  QueryBuilder<Article, Article, QAfterSortBy> thenByBody() {
+    return addSortByInternal('body', Sort.asc);
+  }
+
+  QueryBuilder<Article, Article, QAfterSortBy> thenByBodyDesc() {
+    return addSortByInternal('body', Sort.desc);
+  }
+
+  QueryBuilder<Article, Article, QAfterSortBy> thenByCreatedAt() {
+    return addSortByInternal('createdAt', Sort.asc);
+  }
+
+  QueryBuilder<Article, Article, QAfterSortBy> thenByCreatedAtDesc() {
+    return addSortByInternal('createdAt', Sort.desc);
+  }
+
+  QueryBuilder<Article, Article, QAfterSortBy> thenById() {
+    return addSortByInternal('id', Sort.asc);
+  }
+
+  QueryBuilder<Article, Article, QAfterSortBy> thenByIdDesc() {
+    return addSortByInternal('id', Sort.desc);
+  }
+
+  QueryBuilder<Article, Article, QAfterSortBy> thenByTitle() {
+    return addSortByInternal('title', Sort.asc);
+  }
+
+  QueryBuilder<Article, Article, QAfterSortBy> thenByTitleDesc() {
+    return addSortByInternal('title', Sort.desc);
+  }
+
+  QueryBuilder<Article, Article, QAfterSortBy> thenByUpdatedAt() {
+    return addSortByInternal('updatedAt', Sort.asc);
+  }
+
+  QueryBuilder<Article, Article, QAfterSortBy> thenByUpdatedAtDesc() {
+    return addSortByInternal('updatedAt', Sort.desc);
+  }
+}
+
+extension ArticleQueryWhereDistinct
+    on QueryBuilder<Article, Article, QDistinct> {
+  QueryBuilder<Article, Article, QDistinct> distinctByBody(
+      {bool caseSensitive = true}) {
+    return addDistinctByInternal('body', caseSensitive: caseSensitive);
+  }
+
+  QueryBuilder<Article, Article, QDistinct> distinctByCreatedAt() {
+    return addDistinctByInternal('createdAt');
+  }
+
+  QueryBuilder<Article, Article, QDistinct> distinctById() {
+    return addDistinctByInternal('id');
+  }
+
+  QueryBuilder<Article, Article, QDistinct> distinctByTitle(
+      {bool caseSensitive = true}) {
+    return addDistinctByInternal('title', caseSensitive: caseSensitive);
+  }
+
+  QueryBuilder<Article, Article, QDistinct> distinctByUpdatedAt() {
+    return addDistinctByInternal('updatedAt');
+  }
+}
+
+extension ArticleQueryProperty
+    on QueryBuilder<Article, Article, QQueryProperty> {
+  QueryBuilder<Article, String, QQueryOperations> bodyProperty() {
+    return addPropertyName('body');
+  }
+
+  QueryBuilder<Article, DateTime, QQueryOperations> createdAtProperty() {
+    return addPropertyName('createdAt');
+  }
+
+  QueryBuilder<Article, int?, QQueryOperations> idProperty() {
+    return addPropertyName('id');
+  }
+
+  QueryBuilder<Article, String, QQueryOperations> titleProperty() {
+    return addPropertyName('title');
+  }
+
+  QueryBuilder<Article, DateTime, QQueryOperations> updatedAtProperty() {
+    return addPropertyName('updatedAt');
+  }
+}
