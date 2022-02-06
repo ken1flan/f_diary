030cea495f32efaf582bfe344e02dfdd218a0ad5
ken1flan <ken1flan@gmail.com>
2022-02-06 23:07:35 +0900

    テストをくるんで、自動的に共通の初期化終了処理をできるようにする。

--- test/test_helper.dart
+++ test/test_helper.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'supports/isar_support.dart';
 
 class TestHelper {
+  static void wrapTest(void Function() body) {
+    IsarSupport.initilize();
+
+    body();
+
+    IsarSupport.finalize();
+  }
+
   static Widget wrapWithMaterial(Widget widget) {
     return MaterialApp(
       home: widget,
--- test/widgets/my_home_page_test.dart
+++ test/widgets/my_home_page_test.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:f_diary/widgets/my_home_page.dart';
-
 import '../test_helper.dart';
 
 void main() {
-  const myHomePage = MyHomePage(title: 'タイトル');
+  TestHelper.wrapTest(() async {
+    const myHomePage = MyHomePage(title: 'タイトル');
+    testWidgets('エディットボタンを押したときに記事ページが表示されること', (WidgetTester tester) async {
+      await tester.pumpWidget(TestHelper.wrapWithMaterial(myHomePage));
 
-  testWidgets('エディットボタンを押したときに記事ページが表示されること', (WidgetTester tester) async {
-    await tester.pumpWidget(TestHelper.wrapWithMaterial(myHomePage));
+      await tester.tap(find.byIcon(Icons.edit));
+      await tester.pump();
 
-    await tester.tap(find.byIcon(Icons.edit));
-    await tester.pump();
-
-    var dateTime = DateTime.now();
-    var titleString = '${dateTime.year}-${dateTime.month}-${dateTime.day}';
-    expect(find.text(titleString, skipOffstage: false), findsOneWidget);
+      var dateTime = DateTime.now();
+      var titleString = '${dateTime.year}-${dateTime.month}-${dateTime.day}';
+      expect(find.text(titleString, skipOffstage: false), findsOneWidget);
+    });
   });
 }
