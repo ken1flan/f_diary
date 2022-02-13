f91ac1d695ff5e0e9a2888afea2914b83931c80c
KUDO Ken-ichi <ken1flan@gmail.com>
2022-02-13 10:41:54 +0900

    記事を保存 (#21)
    * 記事を保存
    
    * 新規作成のテストを追加
    
    * 既存の記事を指定したときのテストを追加
    
    * 並列でテストを実行させないようにする
    
    データベースを使っているので、テストを並列で実行されると
    isarのオープンで失敗するため。
    
    * 保存時の処理をまとめる

--- lib/widgets/article_page.dart
+++ lib/widgets/article_page.dart
@@ -1,3 +1,4 @@
+import 'package:f_diary/isar_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:f_diary/models/article.dart';
 
@@ -26,15 +27,32 @@ class _ArticleState extends State<ArticlePage> {
               children: [
                 TextFormField(
                   initialValue: article.title,
+                  key: const ValueKey('articleTitleTextField'),
                   decoration: const InputDecoration(
                       labelText: 'タイトル', hintText: '今日をひとことでいうとなんですか？'),
+                  onChanged: (value) {
+                    article.title = value;
+                    _saveArticle(article);
+                  },
                 ),
                 TextFormField(
                   initialValue: article.body,
+                  key: const ValueKey('articleBodyTextField'),
                   decoration: const InputDecoration(labelText: '内容'),
                   maxLines: 10,
+                  onChanged: (value) {
+                    article.body = value;
+                    _saveArticle(article);
+                  },
                 )
               ],
             )));
+  }
+
+  void _saveArticle(Article article) {
+    article.updatedAt = DateTime.now();
+    isar.writeTxnSync((isar) {
+      article.id = isar.articles.putSync(article);
+    });
   }
 }
---
+++ test/widgets/article_page_test.dart
@@ -0,0 +1,72 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:f_diary/isar_helper.dart';
+import 'package:f_diary/models/article.dart';
+
+import 'package:isar/isar.dart';
+import 'package:f_diary/widgets/article_page.dart';
+import '../test_helper.dart';
+
+void main() {
+  TestHelper.wrapTest(() async {
+    group('未保存の記事を指定されたとき', () {
+      var article = Article()
+        ..title = ''
+        ..body = ''
+        ..createdAt = DateTime.now()
+        ..updatedAt = DateTime.now();
+
+      var articlePage = ArticlePage(article);
+
+      testWidgets('ページが表示されること', (WidgetTester tester) async {
+        await tester.pumpWidget(TestHelper.wrapWithMaterial(articlePage));
+
+        var dateTime = DateTime.now();
+        var titleString = '${dateTime.year}-${dateTime.month}-${dateTime.day}';
+
+        expect(find.text(titleString, skipOffstage: false), findsOneWidget);
+        var savedArticle = isar.articles.where().findFirstSync();
+        expect(savedArticle, equals(null));
+      });
+
+      testWidgets('記入後に自動的に保存されていること', (WidgetTester tester) async {
+        await tester.pumpWidget(TestHelper.wrapWithMaterial(articlePage));
+        await tester.enterText(
+            find.byKey(const ValueKey('articleTitleTextField')), '記事のタイトル');
+        await tester.enterText(
+            find.byKey(const ValueKey('articleBodyTextField')), '記事の本文');
+        var savedArticle = isar.articles.where().findFirstSync();
+        expect(savedArticle?.title, equals('記事のタイトル'));
+        expect(savedArticle?.body, equals('記事の本文'));
+      });
+    });
+  });
+
+  group('既存の記事を指定されたとき', () {
+    var article = Article()
+      ..title = '記事のタイトル'
+      ..body = '記事の本文'
+      ..createdAt = DateTime.now()
+      ..updatedAt = DateTime.now();
+
+    var articlePage = ArticlePage(article);
+
+    testWidgets('ページが表示されること', (WidgetTester tester) async {
+      await tester.pumpWidget(TestHelper.wrapWithMaterial(articlePage));
+
+      expect(find.text('記事のタイトル'), findsOneWidget);
+      expect(find.text('記事の本文'), findsOneWidget);
+    });
+
+    testWidgets('変更後に自動的に保存されていること', (WidgetTester tester) async {
+      await tester.pumpWidget(TestHelper.wrapWithMaterial(articlePage));
+      await tester.enterText(
+          find.byKey(const ValueKey('articleTitleTextField')), '変更後の記事のタイトル');
+      await tester.enterText(
+          find.byKey(const ValueKey('articleBodyTextField')), '変更後の記事の本文');
+      var savedArticle = isar.articles.where().findFirstSync();
+      expect(savedArticle?.title, equals('変更後の記事のタイトル'));
+      expect(savedArticle?.body, equals('変更後の記事の本文'));
+    });
+  });
+}
